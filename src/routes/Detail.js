68391b69a46c0ef837b4565c5e7561f73d2fab19
jiwonss <sjs3326@naver.com>
2020-11-07 03:38:23 +0900

    css 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -14,27 +14,23 @@ class Detail extends React.Component {
     if (location.state) {
       return (
         <div className="detail__container">
-          <div className="movies">
-            <div className="movie">
-              <img
-                src={location.state.poster}
-                alt={location.state.title}
-                title={location.state.title}
-              />
-              <div className="movie__data">
-                <h3 className="movie__title">{location.state.title}</h3>
-                <h5 className="movie__year">{location.state.year}</h5>
-                <ul className="movie__genres">
-                  {location.state.genres.map((genre, index) => (
-                    <li key={index} className="genres__genre">
-                      {genre}
-                    </li>
-                  ))}
-                </ul>
-                <p className="movie__summary">
-                  {location.state.summary.slice(0, 180)}...
-                </p>
-              </div>
+          <div className="detail__movie">
+            <img
+              src={location.state.poster}
+              alt={location.state.title}
+              title={location.state.title}
+            />
+            <div className="movie__data">
+              <h3 className="movie__title">{location.state.title}</h3>
+              <h5 className="movie__year">{location.state.year}</h5>
+              <ul className="movie__genres">
+                {location.state.genres.map((genre, index) => (
+                  <li key={index} className="genres__genre">
+                    {genre}
+                  </li>
+                ))}
+              </ul>
+              <p className="movie__summary">{location.state.summary}</p>
             </div>
           </div>
         </div>
